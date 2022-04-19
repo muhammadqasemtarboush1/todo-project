6d70480308180bbe97a25cf6bdb5766e4bc0bcbd
muhammadqasemtarboush1 <muhammadtarboush1@gmail.com>
2022-04-20 02:52:48 +0300

    add: Js file

---
+++ js/app.js
@@ -0,0 +1,19 @@
+console.log("js runing")
+
+let username = prompt("Enter your Name")
+let gender = prompt("Enter Your Gender  : Male / Female")
+let userAge = prompt("Enter your Age")
+if (userAge <= 0) {
+    alert("You enterd unvalid Age")
+}
+
+confirm("Do you want to see the welcome Message?")
+// debugger
+if (true && gender == "Male" || gender == "male") {
+
+    alert(`Welcom Mr ${username}`)
+} else if (true && gender == "Female" || gender == "Female") {
+    alert(`Welcom Ms ${username}`)
+} else {
+    alert(`Welcome ${username}`)
+}
