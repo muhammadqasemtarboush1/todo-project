21e5ce534f090ffe4a3d1e29083a7c5a910a9d0d
muhammadqasemtarboush1 <muhammadtarboush1@gmail.com>
2022-05-06 18:00:55 +0300

    fix: wrong gender welcoming msg

--- js/app.js
+++ js/app.js
@@ -1,19 +1,25 @@
-console.log("js runing")
+console.log("js runing");
 
-let username = prompt("Enter your Name")
-let gender = prompt("Enter Your Gender  : Male / Female")
-let userAge = prompt("Enter your Age")
+let username = prompt("Enter your Name");
+let gender = prompt("Enter Your Gender  : Male / Female");
+let userAge = prompt("Enter your Age");
 if (userAge <= 0) {
-    alert("You enterd unvalid Age")
+  alert("You enterd unvalid Age");
 }
 
-confirm("Do you want to see the welcome Message?")
+confirm("Do you want to see the welcome Message?");
 // debugger
-if (true && gender == "Male" || gender == "male") {
-
-    alert(`Welcom Mr ${username}`)
-} else if (true && gender == "Female" || gender == "female") {
-    alert(`Welcom Ms ${username}`)
-} else if(true && gender == "Female" || gender == "female"|| gender == "Male" || gender == "male") {
-    alert(`Welcome ${username}`)
+if ((true && gender == "Male") || gender == "male") {
+  alert(`Welcom Mr ${username}`);
+} else if ((true && gender == "Female") || gender == "female") {
+  alert(`Welcom Ms ${username}`);
+} else if (
+  (true && !gender == "Female") ||
+  !gender == "female" ||
+  !gender == "Male" ||
+  !gender == "male"
+) {
+  alert(`Welcome ${username}`);
+} else {
+  alert(`Welcome ${username}`);
 }
